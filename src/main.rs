8ef3718d4da52299db4babdcd6a5c8e9a5bc19ba
Reese Robertson <reese.c.robertson@gmail.com>
2020-07-17 11:41:10 -0700

    Exclude unfinished environment module

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,10 @@ extern crate static_assertions;
 mod constants;
 mod logbook;
 mod valve;
-mod environment;
 mod calendar;
 mod config_persist;
-
-use environment::Environment;
 
 fn main() {
     // Bootstrapping
     pretty_env_logger::init();
-
-    // This owns all service instances
-    let env = Environment::new();
 }
